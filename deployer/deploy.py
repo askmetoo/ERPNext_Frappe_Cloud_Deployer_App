098f83ae613f7ccc63cd29559c58d9ab4f8326ab
Paul Clinton <paul.clinton@promantia.com>
2020-03-10 17:45:25 +0530

    [6738/deployer] Fixed the issue with deploying the doctypes where it was throwing 417 error code and also added some more logs

--- deployer/deploy.py
+++ deployer/deploy.py
@@ -9,27 +9,52 @@ import requests
 import urllib.parse
 
 def startDeployment(**kwargs):
+    ###
+    # This method will prepare the necessary data and call the methods 
+    # which will deploy(upload) the json to the respective doctypes.
+    ###
     try:
+        skipAppList=['frappe', 'erpnext', 'deployer']
         print("Server is "+kwargs['server'])
         apps=frappe.get_installed_apps()
+        exitCriteria=0
         for app in apps:
+            if app in skipAppList:
+                continue
             if os.path.exists(frappe.get_app_path(app,"fixtures")):
-                fixture_files=sorted(os.listdir(frappe.get_app_path(app,"fixtures")))
-                for fname in fixture_files:
+                fixture_file_list=sorted(os.listdir(frappe.get_app_path(app,"fixtures")))
+                file_list_lenght=len(fixture_file_list)
+                checkList=[]
+                while fixture_file_list:
+                    if file_list_lenght == len(fixture_file_list):
+                        checkList=fixture_file_list.copy()
+                        file_List_Lenght=len(fixture_file_list)
+                    fname=file_list_lenght.pop()
                     if fname.endswith(".json"):
                         wholeJsonData=getJsonData(frappe.get_app_path(app,"fixtures",fname))
                         for jsonData in wholeJsonData:
-                            uploadData(jsonData,fname,kwargs)
+                            if not uploadData(jsonData,fname,kwargs):
+                                file_list_lenght.insert(0,fname)
+                                file_List_Lenght=len(fixture_file_list)
+                                break
+                    if checkList == fixture_file_list:
+                        raise Exception("Unable to deploy the following doctypes "+str(fixture_file_list)+". Reason: Some dependent doctypes could be missing.")
     except Exception as err:
         print(err)
-        frappe.logger().error(err)
+        frappe.logger().error(frappe.utils.get_traceback())
 
 def getJsonData(jsonFile):
+    ###
+    # Read the json file and convert it into json object
+    ###
     with open(jsonFile) as f:
         jsonData=json.load(f)
     return jsonData
 
 def uploadData(jsonData,fileName,kwargs):
+    ###
+    # Uploads the json data to the server
+    ###
     userName=kwargs['user']
     password=kwargs['password']
     formatedAuthStr=userName+':'+password
@@ -40,22 +65,31 @@ def uploadData(jsonData,fileName,kwargs):
     }
     api =kwargs['server']+"/api/resource/"+urllib.parse.quote(jsonData['doctype'])
     response = requests.post(api,headers=headers,data=json.dumps(removeUnwantedKeys(jsonData)),cookies=getCookie(kwargs))
+    if response.status_code==417:
+        return False
     if response.status_code==200:
         print("Successfully inserted "+jsonData['name']+" to "+jsonData['doctype'])
         frappe.logger().info("Successfully inserted "+jsonData['name']+" to "+jsonData['doctype'])
     elif response.status_code == 409 and response.reason == 'CONFLICT' :
         response = requests.put(api+"/"+urllib.parse.quote(jsonData['name']),headers=headers,data=json.dumps(removeUnwantedKeys(jsonData)),cookies=getCookie(kwargs))
-        if response.status_code==200:
+        if response.status_code==417:
+            print(response.status_code)
+            return False
+        elif response.status_code==200:
             if not 'data' in response.json():
                 raise Exception("Unable to update the doctype "+jsonData['doctype']+ " Api is :"+api+"/"+urllib.parse.quote(jsonData['name']))
             print("Successfully updated "+jsonData['doctype']+" doctype with "+jsonData['name'] )
             frappe.logger().info("Successfully updated "+jsonData['doctype']+" doctype with "+jsonData['name'] )
         else:
             raise Exception("Exception while processing the request. Response code is "+str(response.status_code)+". Reason is "+response.reason)
-    elif (not response.status_code == 200) and (not response.status_code==417):
+    elif not response.status_code == 200:
         raise Exception("Exception while processing the request. Response code is "+str(response.status_code)+". Reason is "+response.reason)
+    return True
 
 def getCookie(kwargs):
+    ###
+    # Gets the session details from the cookie after login
+    ###
     response = requests.post(kwargs['server']+'/api/method/login', data = {'usr':kwargs['user'], 'pwd':kwargs['password']})
     if response.status_code == 200:
         delim="; "
@@ -71,12 +105,18 @@ def getCookie(kwargs):
         raise Exception("Exception while connecting to the server. Response code is "+str(response.status_code)+". Reason is "+response.reason)
 
 def createJson(tup, di): 
+    ###
+    # Coverts the array to dictionary
+    ###
     for val in tup:
         it = iter(val) 
         di.update(dict(zip(it, it)))
     return di
 
 def removeUnwantedKeys(jsonData):
+    ###
+    # Removes unwanted keys, these keys are not required while sending the data to the server
+    ###
     del_keys=('modified','modified_by', 'creation', 'owner', 'idx')
     for key in del_keys:
         if key in jsonData:
